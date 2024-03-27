00a02ebee661931dc969a3d65bdeb297cd48721a
Xenogil <147083136+Xenogil@users.noreply.github.com>
2024-03-27 18:52:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,38 +11,17 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
-resource "azurerm_resource_group" "example" {
-  name     = format("rg-NAHUM-%d", random_integer.example.result)
-  location = "West Europe"
-}
-
 resource "random_integer" "nah" {
   min = 1000
   max = 9999
 }
 
-resource "azurerm_app_service_plan" "gil" {
-  name                = format("asp-NAHUM-%d", random_integer.example.result)
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  os_type             = "Linux"
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+resource "azurerm_resource_group" "example" {
+  name     = format("rg-NAHUM-%d", random_integer.nah.result)
+  location = "West Europe"
 }
 
-resource "azurerm_linux_web_app" "gio" {
-  name                = format("webapp-NAHUM-%d", random_integer.example.result)
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
 
-  site_config {
-    java_version       = "java17"
-    java_server        = "JAVA"
-    java_server_version = "17"
-  }
-}
+
 
 
