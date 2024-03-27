9e785c86b7531058fa49be96be31b915c7edd661
Xenogil <147083136+Xenogil@users.noreply.github.com>
2024-03-27 18:57:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,13 +21,24 @@ resource "azurerm_resource_group" "nah" {
   location = "West Europe"
 }
 resource "azurerm_app_service_plan" "gil" {
-  name                = format("asp-NAHUM-%d", random_integer.example.result)
+  name                = format("asp-NAHUM-%d", random_integer.nah.result)
   location            = azurerm_resource_group.nah.location
   resource_group_name = azurerm_resource_group.nah.name
   os_type             = "Linux"
   sku {
     tier = "Basic"
     size = "B1"
+  }
+}
+resource "azurerm_linux_web_app" "gio" {
+  name                = format("webapp-NAHUM-%d", random_integer.nah.result)
+  location            = azurerm_resource_group.nah.location
+  resource_group_name = azurerm_resource_group.nah.name
+
+  site_config {
+    java_version       = "java17"
+    java_server        = "JAVA"
+    java_server_version = "17"
   }
 }
 
