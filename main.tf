6c74a7046c5ae27abdc860e87951ac6d7cd95d6e
Xenogil <147083136+Xenogil@users.noreply.github.com>
2024-03-27 18:54:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,16 @@ resource "azurerm_resource_group" "example" {
   name     = format("rg-NAHUM-%d", random_integer.nah.result)
   location = "West Europe"
 }
+resource "azurerm_app_service_plan" "gil" {
+  name                = format("asp-NAHUM-%d", random_integer.example.result)
+  location            = azurerm_resource_group.nah.location
+  resource_group_name = azurerm_resource_group.nah.name
+  os_type             = "Linux"
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
 
 
 
